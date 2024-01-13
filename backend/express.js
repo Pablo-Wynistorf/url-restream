350a044755ab306bed0e099ac0d3c724ff6abe9b
Pablo-Wynistorf <p.wynistorf@gmail.com>
2024-01-13 19:19:39 +0100

    Adding files

---
+++ backend/express.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+if (process.env.VCAP_SERVICES) {
+  const services = JSON.parse(process.env.VCAP_SERVICES);
+  if (Object.keys(services).length !== 0) {
+    const cd = services["mongodbent"][0].credentials;
+    process.env.DATABASE_URI = cd.database_uri;
+  }
+}
+
+const API_PORT = process.env.API_PORT;
+const API_URL = process.env.API_URL;
+const DATABASE_URI = process.env.DATABASE_URI;
+
+function connectToDatabase() {
+  mongoose.connect(DATABASE_URI);
+}
+
+connectToDatabase();
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('MongoDB connection error. Reconnecting...');
+  setTimeout(connectToDatabase, 5000);
+
+});
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected. Reconnecting...');
+  connectToDatabase();
+});
+
+db.on('econnrefused', () => {
+  console.log('MongoDB connection refused. Reconnecting...');
+  setTimeout(connectToDatabase, 5000);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+const { Schema } = mongoose;
+const urlSchema = new Schema({
+  link: String,
+  randomString: String,
+});
+
+const urlDB = mongoose.model('urldb', urlSchema);
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState !== 1) {
+    connectToDatabase();
+  }
+  next();
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const validateURL = (url) => {
+  const urlRegex = /^(https?:\/\/)?([\w\d-]+\.)*[\w\d]+\.[\w]{2,}(\/.*)*$/;
+  return urlRegex.test(url);
+};
+
+app.post('/api/link', async (req, res) => {
+  const { link } = req.body;
+
+  const validatedLink = link.startsWith('http://') || link.startsWith('https://') ? link : `https://${link}`;
+
+  if (!validateURL(validatedLink)) {
+    return res.status(400).json({ success: false });
+  }
+
+  try {
+    const existingUser = await urlDB.findOne({ link: validatedLink });
+
+    if (existingUser) {
+      return res.status(200).json({ success: true, shortenedLink: `${API_URL}/${existingUser.randomString}` });
+    }
+
+    const randomString = crypto.randomBytes(3).toString('hex').slice(0, 6);
+
+    const newUser = await urlDB.findOneAndUpdate(
+      { randomString },
+      { $setOnInsert: { link: validatedLink, randomString } },
+      { upsert: true, new: true }
+    );
+
+    const shortenedLink = `${API_URL}/${newUser.randomString}`;
+    res.status(200).json({ success: true, shortenedLink });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong, try again later' });
+  }
+});
+
+app.all('/:refCode', async (req, res) => {
+  const { refCode } = req.params;
+
+  try {
+    const urlDoc = await urlDB.findOne({ randomString: refCode });
+
+    if (urlDoc && urlDoc.link) {
+      const url = new URL(urlDoc.link);
+      res.redirect(url.href);
+    } else {
+      res.redirect('/');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Something went wrong, try again later' });
+  }
+});
+
+app.listen(API_PORT, () => {
+  console.log('Login API started on port', API_PORT);
+});
---
+++ backend/public/index.js
@@ -0,0 +1,73 @@
+const sendButton = document.getElementById('sendButton');
+const urlInput = document.getElementById('url');
+const inputContainer = document.getElementById('inputContainer');
+const title2 = document.getElementById('title-2');
+const errorBox = document.createElement('div');
+const successBox = document.createElement('div');
+const newLink = document.createElement('button');
+errorBox.className = 'error-box';
+successBox.className = 'success-box';
+newLink.className = 'new-link-box';
+newLink.textContent = 'Shorten another link';
+newLink.style.display = 'none';
+let isCopyMode = false;
+
+sendButton.addEventListener('click', async () => {
+    if (isCopyMode === false) {
+        try {
+            const originalUrl = urlInput.value;
+            const response = await fetch('/api/link', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ link: originalUrl }),
+            });
+
+            if (response.ok) {
+                isCopyMode = true;
+                const data = await response.json();
+                urlInput.value = data.shortenedLink;
+                urlInput.readOnly = true;
+                title2.innerHTML = 'Your shortened URL';
+                sendButton.textContent = 'Copy';
+                sendButton.id = 'copyButton';
+                inputContainer.appendChild(sendButton);
+                newLink.style.display = 'block'; // Show the "New Link" button
+            } else {
+                sendButton.disabled = true;
+                displayError('Error: Enter a valid URL');
+            }
+        } catch (error) {
+            console.error('Error during fetch:', error);
+        }
+    } else {
+        urlInput.select();
+        document.execCommand('copy');
+        sendButton.style = 'background-color: #248046; background-color:hover: #23a559;';
+        displaySuccess('Success: Link copied!');
+    }
+});
+
+newLink.addEventListener('click', () => {
+    window.location.reload();
+});
+
+function displayError(errorMessage) {
+    errorBox.textContent = errorMessage;
+    document.body.appendChild(errorBox);
+    setTimeout(() => {
+        errorBox.remove();
+        sendButton.disabled = false;
+    }, 2500);
+}
+
+function displaySuccess(successMessage) {
+    successBox.textContent = successMessage;
+    document.body.appendChild(successBox);
+    document.body.appendChild(newLink);
+    setTimeout(() => {
+        successBox.remove();
+        sendButton.disabled = false;
+    }, 2500);
+}
